7df884bd532769cfd7ab43b4bf03747b0d2fec60
amadijeffrey <amadichibuike72@gmail.com>
2022-04-11 21:07:03 +0100

    fixed a bug

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ $('#downloadbtn').click(() => {
             var courses = document.getElementsByClassName('course');
             var creditUnits = document.getElementsByClassName('credit-unit');
             var grades = document.getElementsByClassName('grade');
-            document.querySelector('tbody').insertAdjacentHTML('beforeend', `<tr><td>${courses[i].value}</td><td>${creditUnits[i].value}</td><td>${grades[i].value.toUpperCase()}</td><td>${gradeValues[i]}</td><td>${qualityPoints[i]}</td></tr>`)
+            document.querySelector('tbody').insertAdjacentHTML('beforeend', `<tr><td>${courses[i].value}</td><td>${creditUnits[i].value}</td><td>${grades[i].value.toUpperCase()}</td><td>${gradePoints[i]}</td><td>${qualityPoints[i]}</td></tr>`)
         }
     }
 
@@ -147,3 +147,113 @@ function findQualityPoint(gradeValue,creditUnit){
     
 }
 
+
+var datass = '';
+var DataArr = [];
+PDFJS.workerSrc = '';
+
+function ExtractText() {
+    var input = document.getElementById("file-id");
+    var fReader = new FileReader();
+    fReader.readAsDataURL(input.files[0]);
+    // console.log(input.files[0]);
+    fReader.onloadend = function (event) {
+        convertDataURIToBinary(event.target.result);
+    }
+}
+
+var BASE64_MARKER = ';base64,';
+
+function convertDataURIToBinary(dataURI) {
+
+    var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+    var base64 = dataURI.substring(base64Index);
+    var raw = window.atob(base64);
+    var rawLength = raw.length;
+    var array = new Uint8Array(new ArrayBuffer(rawLength));
+
+    for (var i = 0; i < rawLength; i++) {
+        array[i] = raw.charCodeAt(i);
+    }
+    pdfAsArray(array)
+
+}
+
+function getPageText(pageNum, PDFDocumentInstance) {
+    // Return a Promise that is solved once the text of the page is retrieved
+    return new Promise(function (resolve, reject) {
+        PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
+            // The main trick to obtain the text of the PDF page, use the getTextContent method
+            pdfPage.getTextContent().then(function (textContent) {
+                var textItems = textContent.items;
+                var finalString = "";
+
+                // Concatenate the string of the item to the final string
+                for (var i = 0; i < textItems.length; i++) {
+                    var item = textItems[i];
+
+                    finalString += item.str + " ";
+                }
+
+                // Solve promise with the text retrieven from the page
+                resolve(finalString);
+            });
+        });
+    });
+}
+
+function pdfAsArray(pdfAsArray) {
+
+    PDFJS.getDocument(pdfAsArray).then(function (pdf) {
+
+        var pdfDocument = pdf;
+        // Create an array that will contain our promises
+        var pagesPromises = [];
+
+        for (var i = 0; i < pdf.pdfInfo.numPages; i++) {
+            // Required to prevent that i is always the total of pages
+            (function (pageNumber) {
+                // Store the promise of getPageText that returns the text of a page
+                pagesPromises.push(getPageText(pageNumber, pdfDocument));
+            })(i + 1);
+        }
+
+        // Execute all the promises
+        Promise.all(pagesPromises).then(function (pagesText) {
+
+            // Display text of all the pages in the console
+            // e.g ["Text content page 1", "Text content page 2", "Text content page 3" ... ]
+        
+            var outputStr = "";
+            for (var pageNum = 0; pageNum < pagesText.length; pageNum++) {
+                outputStr = "";
+                outputStr = "<br/><br/>Page " + (pageNum + 1) + " contents <br/> <br/>";
+
+                  const allValues =  pagesText[pageNum].split(' ')
+                  const endValue = allValues.length - 9
+                  const selectedValues = allValues.slice(6,endValue)
+                  for(let i = 3;i<selectedValues.length;i+=3){
+                      selectedValues.splice(i,2)
+                  }
+                   numCoursesInPDF = selectedValues.length / 3
+                   document.getElementById('courses-container').innerHTML = '';
+
+                  for (var i = 1; i <= numCoursesInPDF; i++) {
+                    document.getElementById('courses-container').insertAdjacentHTML("beforeend", `<input class="course animated fadeIn" placeholder="COURSE ${i + 1}" type="text">  <input type="number" placeholder="CREDIT UNIT" class="credit-unit animated fadeIn">  <input type="text" placeholder="GRADE" class="grade animated fadeIn"<br/><hr/><br/>`);
+                }
+                 const inputs = document.querySelectorAll('.fadeIn')
+                 for(let i = 0;i < inputs.length; i++){
+                     inputs[i].value = selectedValues[i]
+                 }
+            }
+            window.scrollTo(0,document.body.scrollHeight);
+
+
+        });
+        uploadedADocument = true
+
+    }, function (reason) {
+        // PDF loading error
+        console.error(reason);
+    });
+}
